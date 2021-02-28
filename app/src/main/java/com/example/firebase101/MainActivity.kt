58b8b250e1978faf931901f21aed45ef07e048fd
Mbesmek <m.besmek@gmail.com>
2021-02-28 09:41:30 +0300

    added logout user

--- app/src/main/java/com/example/firebase101/MainActivity.kt
+++ app/src/main/java/com/example/firebase101/MainActivity.kt
@@ -1,15 +1,91 @@
 package com.example.firebase101
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var authStateListener: FirebaseAuth.AuthStateListener
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initAuthStateListener()
 
 
     }
 
+    private fun initAuthStateListener() {
+        authStateListener=object :FirebaseAuth.AuthStateListener{
+            override fun onAuthStateChanged(p0: FirebaseAuth) {
+                var user =p0.currentUser
+                if(user!=null){
+
+                }else{
+                    var intent = Intent(this@MainActivity,LoginActivity::class.java)
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK) // user can't back to main menu
+                    startActivity(intent)
+                    finish()
+                }
+
+            }
+
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.mainmenu,menu)
+        return super.onCreateOptionsMenu(menu)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item?.itemId){
+            R.id.menuLogout->{
+                logOut()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun logOut() {
+        FirebaseAuth.getInstance().signOut()
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        controlUser()
+    }
+        // Control the user come by back button
+    //if user come by back button system remove the user
+    private fun controlUser() {
+        var user= FirebaseAuth.getInstance().currentUser
+        if (user==null){
+            var intent = Intent(this@MainActivity,LoginActivity::class.java)
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK) // user can't back to main menu
+            startActivity(intent)
+            finish()
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        FirebaseAuth.getInstance().addAuthStateListener (authStateListener)
+    }
+
+    override fun onStop() {
+        super.onStop()
+    if (authStateListener!=null){
+        FirebaseAuth.getInstance().removeAuthStateListener (authStateListener)
+    }
+    }
 
 }
